19e2ce71265019831354f0f7e8d31f2833d09fce
Cullen Haser <cchauser@users.noreply.github.com>
2020-10-23 17:11:29 -0400

    Lemmatizer
    Added a lemmatizer for keyword analysis

--- MasterConsumer.py
+++ MasterConsumer.py
@@ -14,12 +14,17 @@ import itertools
 import multiprocessing as mp
 import MySQLdb as mysql
 import numpy as np
-
 import pickle
+import nltk
+
 from keras.models import load_model
 from tensorflow.python.keras.preprocessing.sequence import pad_sequences
 from kafka import KafkaConsumer
 from nltk import word_tokenize, FreqDist
+from nltk.stem import WordNetLemmatizer, SnowballStemmer
+from nltk.stem.porter import *
+
+nltk.download('wordnet')
 
 #You'll have to change your working directory if you run it on your own machine
 wdir = '/home/cullen'
@@ -78,7 +83,7 @@ class masterConsumer(object):
                 #Stop updating the sentiment and activity change after a set amount of time.
                 if int(time.time()) - tweet[0]['time'] > 300:
                     userBuffer[topic].remove(tweet)
-                
+                    
     
     def runConsumer(self):
         with open('{}/models/extendedStopwords.txt'.format(wdir)) as f:
@@ -86,8 +91,11 @@ class masterConsumer(object):
         
         for i in range(len(extendedStopwords)):
             extendedStopwords[i] = extendedStopwords[i].replace('\n', '')
+            
+        wnl = WordNetLemmatizer()
+        stemmer = SnowballStemmer('english')
         
-        pollTimeSeconds = 180 # let it buffer 1 minute for every topic
+        pollTimeSeconds = 180 
         textBuffer = {}
         locationBuffer = {}
         searchTerms = {}
@@ -108,8 +116,6 @@ class masterConsumer(object):
                         userPacket = {'time': message.value['time'], 'user': message.value['user'], 
                                       'tweet': message.value['tweet'], 'deltasentiment': 0, 'deltaactivity': 0}
                         self.sqlInsert(message.value['topic'][1], 'user', userPacket)
-                        
-
                     #We don't save tweets from target users that happen before we have a chance to get a baseline sentiment.
                     #This only occurs in the pollTimeSeconds window between consumer start up and the first batch being consumed
                 else:
@@ -119,7 +125,7 @@ class masterConsumer(object):
                         textBuffer[message.value['topic'][0]].append(text)
                         locationBuffer[message.value['topic'][0]].append(message.value['location'])
                     else:
-                        self.createTables(message.value['topic'][0]) #First time seeing a topic create the table!
+                        self.createTables(message.value['topic'][0]) #First time seeing a topic, create the table!
                         
                         textBuffer[message.value['topic'][0]] = [text]
                         locationBuffer[message.value['topic'][0]] = [message.value['location']]
@@ -127,7 +133,10 @@ class masterConsumer(object):
 
               
             currTime = int(time.time()) #Use int for database indexing purposes
-    
+            #Get rid of empty topics before analyzing
+            for topic in textBuffer:
+                if len(textBuffer[topic]) == 0:
+                    textBuffer.pop(topic, None)
             #Evaluate the buffer according to the pollTimeSeconds variable
             if currTime - previousPollTime >= pollTimeSeconds:
                 print('Consumption rate: ', self.consumer.metrics()['consumer-fetch-manager-metrics']['records-consumed-rate'])
@@ -135,12 +144,7 @@ class masterConsumer(object):
                 # processList is a container where we'll put all of the new processes that we spawn so we can keep track of them
                 processList = []
                 for topic in textBuffer:
-                    if len(textBuffer[topic]) == 0:
-                        textBuffer.pop(topic, None)
-                        continue
-                    #TODO: Add a process checker to make sure none of the processes are hanging
-                    
-                    
+
                     ### SENTIMENT
                     #Pads and tokenizes tweets for input into the model. Padding value is 0 by default
                     #Output should look like: [[0,0,0,0,...0,32,43,5,432],...[0,0,...0,4234,554,43]]
@@ -165,12 +169,37 @@ class masterConsumer(object):
                     
                     
                     ### WORD FREQUENCY
-                    allwords = [word_tokenize(comment) for comment in textBuffer[topic]]
-                    word_freq = FreqDist(itertools.chain(*allwords))
+                    tokenizedTweets = [word_tokenize(comment) for comment in textBuffer[topic]]
+                    unstemDictCount = {}
+                    # Dictionary lookup for stemmed words to unstemmed words
+                    unstemDict = {} # = {'stemmedWord: [originalWord, count], ...}
+                    #Reduce to base words (ie: dogs to dog, voting to vote)
+                    #Also generate a dictionary to unstem words
+                    for tweet in tokenizedTweets:
+                        for i in range(len(tweet)):
+                            stemmed = stemmer.stem(wnl.lemmatize(tweet[i]))
+                            if stemmed in unstemDictCount:
+                                if tweet[i] in unstemDictCount[stemmed]:
+                                    unstemDictCount[stemmed][tweet[i]][1] += 1
+                                else:
+                                    unstemDictCount[stemmed][tweet[i]] = [tweet[i] , 1]
+                            else:
+                                unstemDictCount[stemmed] = {}
+                                unstemDictCount[stemmed][tweet[i]] = [tweet[i], 1]
+                                unstemDict[stemmed] = [tweet[i], 0]
+                            
+                            if unstemDict[stemmed][0] == tweet[i]:
+                                unstemDict[stemmed][1] += 1
+                            elif unstemDictCount[stemmed][tweet[i]][1] > unstemDict[stemmed][1]:
+                                unstemDict[stemmed] = unstemDictCount[stemmed][tweet[i]]
+                            
+                            tweet[i] = stemmed
+
+                    word_freq = FreqDist(itertools.chain(*tokenizedTweets))
                     #Vocab is list of tuples: [(word, frequency), ...]
                     vocab = word_freq.most_common(50+len(searchTerms[topic])) # searchTerms will always be towards the top of the frequency distribution. This always returns top N non-search-terms
                     i = 0
-                    keywordPacket = []
+                    keywordPacketList = []
                     while i < len(vocab):
                         if vocab[i][0] in searchTerms[topic] or vocab[i][0] in extendedStopwords:
                             vocab.pop(i)
@@ -181,28 +210,29 @@ class masterConsumer(object):
                             continue
                         except:
                             pass
-                        keywordPacket.append({'time': currTime,
-                                              'word': vocab[i][0],
-                                              'times_seen': vocab[i][1],
-                                              'negative': 0,
-                                              'positive': 0,
-                                              'neutral': 0})
+                        keywordPacketList.append({'time': currTime,
+                                                  'word': unstemDict[vocab[i][0]][0],
+                                                  'times_seen': vocab[i][1],
+                                                  'negative': 0,
+                                                  'positive': 0,
+                                                  'neutral': 0})
                         #This handles the determination of how many times a keyword is used in a positive, negative, or neutral context
-                        for tweet in range(len(allwords)):
-                            if vocab[i][0] in allwords[tweet]:
+                        for tweet in range(len(tokenizedTweets)):
+                            if vocab[i][0] in tokenizedTweets[tweet]:
                                 if p[tweet] < .4:
-                                    keywordPacket[-1]['negative'] += allwords[tweet].count(vocab[i][0])
+                                    keywordPacketList[-1]['negative'] += tokenizedTweets[tweet].count(vocab[i][0])
                                 elif p[tweet] > .6:
-                                    keywordPacket[-1]['positive'] += allwords[tweet].count(vocab[i][0])
+                                    keywordPacketList[-1]['positive'] += tokenizedTweets[tweet].count(vocab[i][0])
                                 else:
-                                    keywordPacket[-1]['neutral'] += allwords[tweet].count(vocab[i][0])
+                                    keywordPacketList[-1]['neutral'] += tokenizedTweets[tweet].count(vocab[i][0])
                         i += 1
 
                     #Spawn a new process to insert all of the keywords into the SQL table then add it to process array
-                    kwProcess = mp.Process(target = self.multipleInsertProcessFunction, args = [topic, 'keyword', keywordPacket])
+                    kwProcess = mp.Process(target = self.multipleInsertProcessFunction, args = [topic, 'keyword', keywordPacketList])
                     kwProcess.start()
                     processList.append(kwProcess)
                     
+                    ### LOCATION
                     locationIndex = {}
                     for i in range(len(locationBuffer[topic])):
                         location = locationBuffer[topic][i]
@@ -227,6 +257,11 @@ class masterConsumer(object):
                     locProcess = mp.Process(target = self.multipleInsertProcessFunction, args = [topic, 'location', locationPacket])
                     locProcess.start()
                     processList.append(locProcess)
+                    
+                    # TOPIC MODELING
+                    
+                    
+                    
 
                     textBuffer[topic].clear()
                     locationBuffer[topic].clear()
